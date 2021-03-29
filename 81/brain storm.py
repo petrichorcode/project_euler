0464e1ee0041e751526b7034c6bef23fdfd8c6a1
simtbak <simtbak@users.noreply.github.com>
2021-03-29 19:46:52 +0100

    move into folders

---
+++ 81/brain storm.py
@@ -0,0 +1,19 @@
+
+sample_size = 10 
+start_point = 0
+    for x in range (start_point, start_point + sample_size + 1):
+        #get the average
+        #for...
+    start_point += sample_size
+    
+    
+sample_size = 10
+len(data) = 80
+patch = len(date) / sample_size #= 8 
+0,10
+11,20
+21,30
+31,40
+41,50
+
+
